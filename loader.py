a30d932173acec6224524cd09417e41d7a5c7bc7
Jaron <ngtrio@protonmail.com>
2022-04-10 13:07:18 +0800

    add type

--- loader.py
+++ loader.py
@@ -1,7 +1,5 @@
 import json
-from operator import ge
 import os
-from typing import List
 from PIL import Image
 
 recipe_json = dict()
@@ -72,33 +70,58 @@ def load():
     load_tran()
     load_icon()
 
-def get_tran(name: str) -> str:
+def _get_tran(name: str) -> str:
     if name not in name_tran:
         return name
     return name_tran[name]
 
-def get_cost_per_second(name: str, num: float):
+def get_cost_per_second(name: str, num: float, furnace: str, assembling_machine: str, drill: str, diff: str):
     rec = []
     ingres = []
-    rec.append([f'{get_tran(name)}({name})', num])
 
     if name not in recipe_json:
+        if name in ['stone', 'copper-ore', 'coal', 'iron-ore']:
+            rec.append([_get_tran_name(name), num, _get_tran_name(drill), _get_mining_drill_num(num, drill)])
+        else:
+            rec.append([_get_tran_name(name), num, f'{name}-TODO', 1])
         return rec
 
     recipe = recipe_json[name]
-    ingreJson = []
-    if 'ingredients' in recipe :
-        ingreJson = recipe['ingredients']
-    elif 'normal' in recipe:
-        ingreJson = recipe['normal']['ingredients']
-    else:
-        return rec
+
+    energy_required = 0.5
+    if 'energy_required' in recipe:
+        energy_required = recipe['energy_required']
+    elif diff in recipe:
+        if 'energy_required' in recipe[diff]:
+            energy_required = recipe[diff]['energy_required']
 
     result_count = 1
     if 'result_count' in recipe:
         result_count = recipe['result_count']
     elif 'results' in recipe:
         result_count = recipe['results'][0]['amount']
+    elif diff in recipe:
+        if 'result_count' in recipe[diff]:
+            result_count = recipe[diff]['result_count']
+
+    if 'category' in recipe:
+        # TODO other category
+        if recipe['category'] == 'smelting':
+            rec.append([_get_tran_name(name), num, _get_tran_name(furnace), _get_furnace_num(num, energy_required, result_count, furnace)])
+        elif recipe['category'] == 'chemistry':
+            rec.append([_get_tran_name(name), num, _get_tran_name('chemical-plant'), _get_chemical_plant_num(num, energy_required, result_count)])
+        else:
+            rec.append([_get_tran_name(name), num, _get_tran_name(assembling_machine), _get_assemble_mechine_num(num, energy_required, result_count, assembling_machine)])
+    else:
+        rec.append([_get_tran_name(name), num, _get_tran_name(assembling_machine), _get_assemble_mechine_num(num, energy_required, result_count, assembling_machine)])
+
+    ingreJson = []
+    if 'ingredients' in recipe :
+        ingreJson = recipe['ingredients']
+    elif diff in recipe:
+        ingreJson = recipe[diff]['ingredients']
+    else:
+        return rec
 
     for i in ingreJson:
         ingredients_name = ''
@@ -112,7 +135,7 @@ def get_cost_per_second(name: str, num: float):
 
         
         value = ingredients_num / result_count * num
-        sub_ingredients = get_cost_per_second(ingredients_name, value)
+        sub_ingredients = get_cost_per_second(ingredients_name, value, furnace, assembling_machine, drill, diff)
         ingres.append(sub_ingredients)
 
     if len(ingres) > 0:
@@ -120,30 +143,81 @@ def get_cost_per_second(name: str, num: float):
     return rec
 
 def printRecipe(recipe, depth):
-    all = dict()
+    item = dict()
+    factory = dict()
     prefix = '-'*depth
     if len(recipe)  <= 0 or len(recipe[0]) <= 0: 
         return
     name = recipe[0][0]
     num = recipe[0][1]
-    print(f'{prefix}{name} x {num:.2f}')
-    all[name] = num
+    factory_name = recipe[0][2]
+    factory_num = recipe[0][3]
+    print(f'{prefix}{name} x {num:.2f} with {factory_name} x {factory_num:.2f}')
+    item[name] = num
+    factory[factory_name] = factory_num
     if len(recipe) > 1:
         for i in recipe[1]:
-            sub = printRecipe(i, depth + 1)
-            for k in sub:
-                if k in all:
-                    all[k] += sub[k]
+            sub_item, sub_factory = printRecipe(i, depth + 1)
+            for k in sub_item:
+                if k in item:
+                    item[k] += sub_item[k]
                 else:
-                    all[k] = sub[k]
+                    item[k] = sub_item[k]
+            for k in sub_factory:
+                if k in factory:
+                    factory[k] += sub_factory[k]
+                else:
+                    factory[k] = sub_factory[k]
     if depth == 0:
-        print('Total:')
-        for k in all:
-            print(f'{k} x {all[k]:.2f}')
-    return all
+        print('Total Items:')
+        for k in item:
+            print(f'{k} x {item[k]:.2f}')
+        print('Total Factories:')
+        for k in factory:
+            print(f'{k} x {factory[k]:.2f}')
+    return item, factory
+
+def _get_tran_name(name: str) -> str:
+    return f'{_get_tran(name)}({name})'
+
+def _get_furnace_num(item_num: float, energy_required: float, result_count: float, furnace: str) -> float:
+    energy = 1
+    if furnace == 'stone-furnace':
+        energy = 1
+    elif furnace == 'steel-furnace':
+        energy = 2
+    elif furnace == 'electric-furnace':
+        energy = 2
+    else:
+        print('[ERROR] invalid furnace')
+    return item_num / energy * energy_required / result_count
+
+def _get_assemble_mechine_num(item_num: float, energy_required: float, result_count: float, machine: str) -> float:
+    energy = 0.5
+    if machine == 'assembling-machine-1':
+        energy = 0.5
+    elif machine == 'assembling-machine-2':
+        energy = 0.75
+    elif machine == 'assembling-machine-3':
+        energy = 1.25
+    else:
+        print('[ERROR] invalid assembling machine')
+    return item_num / energy * energy_required / result_count
+
+def _get_mining_drill_num(item_num: float, drill: str) -> float:
+    if drill == 'burner-mining-drill':
+        return item_num / 0.25
+    elif drill == 'electric-mining-drill':
+        return item_num / 0.5
+    else:
+        print('[ERROR] invalid drill')
+        return 0
+
+def _get_chemical_plant_num(item_num: float, energy_required: float, result_count: float) -> float:
+    return item_num * energy_required / result_count
 
 if __name__ == '__main__':
     load()
-    recipe = get_cost_per_second('production-science-pack', 1)
+    recipe = get_cost_per_second('production-science-pack', 1, 'steel-furnace', 'assembling-machine-1', 'electric-mining-drill', 'normal')
     printRecipe(recipe, 0)
 
